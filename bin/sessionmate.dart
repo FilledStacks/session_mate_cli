eeeab9a1813f22fab21acb4cd9e344eff4608557
Dane Mackier <dane.mackier@outlook.com>
2023-10-24 16:11:05 +0200

    Updates sweetcore to the latest

--- bin/sessionmate.dart
+++ bin/sessionmate.dart
@@ -38,25 +38,16 @@ Future<void> main(List<String> arguments) async {
 
   try {
     final argResults = runner.parse(arguments);
-    // await _handleFirstRun();
 
     if (argResults[ksVersion]) {
       await _handleVersion();
       exit(0);
     }
 
-    // if (_handleAnalytics(argResults)) exit(0);
-
-    // await _notifyNewVersionAvailable(arguments: arguments);
-
     runner.run(arguments);
   } catch (e, _) {
     stdout.writeln(e.toString());
-    // locator<AnalyticsService>().logExceptionEvent(
-    //   runtimeType: e.runtimeType.toString(),
-    //   message: e.toString(),
-    //   stackTrace: s.toString(),
-    // );
+
     exit(2);
   }
 }
@@ -64,64 +55,4 @@ Future<void> main(List<String> arguments) async {
 /// Prints version of the application.
 Future<void> _handleVersion() async {
   stdout.writeln(await locator<PubService>().getCurrentVersion());
-}
-
-/// Enables or disables sending of analytics data.
-// bool _handleAnalytics(ArgResults argResults) {
-//   if (argResults[ksEnableAnalytics]) {
-//     locator<AnalyticsService>().enable(true);
-//     return true;
-//   }
-
-//   if (argResults[ksDisableAnalytics]) {
-//     locator<AnalyticsService>().enable(false);
-//     return true;
-//   }
-
-//   return false;
-// }
-
-/// Allows user decide to enable or not analytics on first run.
-// Future<void> _handleFirstRun() async {
-//   // final analyticsService = locator<AnalyticsService>();
-//   // if (!analyticsService.isFirstRun) return;
-
-//   stdout.writeln('''
-//   ┌──────────────────────────────────────────────────────────────────┐
-//   │                 Welcome to the Session Mate CLI!                 │
-//   ├──────────────────────────────────────────────────────────────────┤
-//   │ We would like to collect anonymous                               │
-//   │ usage statistics in order to improve the tool.                   │
-//   │                                                                  │
-//   │ Would you like to opt-into help us improve?                      │
-//   └──────────────────────────────────────────────────────────────────┘
-//   ''');
-//   stdout.write('[y/n]: ');
-
-//   final opt = stdin.readLineSync()?.toLowerCase().trim();
-//   analyticsService.enable(opt == 'y' || opt == 'yes');
-
-//   stdout.writeln();
-// }
-
-/// Notifies new version of Session Mate CLI is available
-Future<void> _notifyNewVersionAvailable({
-  List<String> arguments = const [],
-  List<String> ignored = const [kCommandUpdateName],
-}) async {
-  if (arguments.isEmpty) return;
-
-  for (var arg in ignored) {
-    if (arguments.first == arg) return;
-  }
-
-  if (await locator<PubService>().hasLatestVersion()) return;
-
-  stdout.writeln('''
-  ┌──────────────────────────────────────────────────────────────────┐
-  │ A new version of Session Mate CLI is available!                  │
-  │                                                                  │
-  │ To update to the latest version, run "session_mate update"       │
-  └──────────────────────────────────────────────────────────────────┘
-  ''');
 }
