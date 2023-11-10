f6526630a47199fd5a0c1b5b7020a4a6d0e12b0f
Fernando Ferrara <ferrarafer@gmail.com>
2023-11-09 23:11:51 -0300

    chore: add FirebaseService

--- lib/src/locator.dart
+++ lib/src/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:session_mate_cli/src/services/brew_service.dart';
+import 'package:session_mate_cli/src/services/firebase_service.dart';
 import 'package:session_mate_cli/src/services/logger_service.dart';
 import 'package:session_mate_cli/src/services/path_service.dart';
 import 'package:session_mate_cli/src/services/process_service.dart';
@@ -14,4 +15,8 @@ Future setupLocator() async {
   locator.registerLazySingleton(() => PathService());
   locator.registerLazySingleton(() => PubService());
   locator.registerLazySingleton(() => BrewService());
+
+  final firebaseService = FirebaseService();
+  await firebaseService.init();
+  locator.registerSingleton(firebaseService);
 }
